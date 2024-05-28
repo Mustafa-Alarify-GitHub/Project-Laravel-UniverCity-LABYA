17946c65a3ba3d2bafe3781ff277d8477cb38f6b
Mustafa Al-arify <m2024u775516215@gmail.com>
2024-05-28 15:54:11 +0300

    make 2 route in API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Catogry;
+use App\Models\Subject;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+| @method GET
+| @route  /get-all-catogries
+| @des    Get all data from catogries
+*/
+Route::get('/get-all-catogries', function () {
+    $data = Catogry::all();
+    return response()->json(["status" => 200, "data" => $data]);
+});
+/*
+| @method GET
+| @route /get-books/{id}
+| @des   Get Book by ID ()
+*/
+Route::get('/get-books/{id}', function ($id) {
+    $data =  Subject::where("id_catogry", $id)->get();
+    return response()->json(["status" => 200, "data" => $data]);
+});
+// 
